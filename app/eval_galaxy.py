b2d4d255dbfcb7a02ac5cf0c2e36457954c0ffb3
cormoran <cormoran707@gmail.com>
2020-07-19 20:07:06 +0900

    change shipId

--- app/eval_galaxy.py
+++ app/eval_galaxy.py
@@ -3,7 +3,15 @@ import traceback
 
 sys.setrecursionlimit(999999)
 
-from interpreter import Interpreter
+from interpreter import Interpreter, evaluate_all
+import operations as op
+
+
+def get(index: int, node: op.Cons):
+    if index == 0:
+        return op.Ap(op.Car(), node).evaluate(op.Environment())
+    return get(index - 1, op.Ap(op.Cdr(), node))
+
 
 interpreter = Interpreter()
 
@@ -18,6 +26,9 @@ with open('./app/galaxy.txt', 'r') as f:
     #     interpreter.evaluate_expression(
     #         "ap ap ap interact galaxy nil ap ap vec 0 0").print())
 
-    print(
-        interpreter.evaluate_expression(
-            "ap ap ap interact galaxy nil ap ap vec 1 1").print())
+    res = interpreter.evaluate_expression(
+        "ap ap ap interact galaxy nil ap ap vec 1 1")
+    newState = get(0, res)
+    image = get(1, res)
+    print(evaluate_all(interpreter.env, get(0, image)).print())
+    print(evaluate_all(interpreter.env, get(1, image)).print())
